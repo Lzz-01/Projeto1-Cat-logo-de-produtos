086483c12d6de6a7e21b8dc2459aba5dad1f7d6a
Aluno <e023aluno@outlook.com>
2026-08-12 20:22:57 -0300

    Backend

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const produtoRoutes = require("./routes/produtoRoutes");
+
+const app = express();
+const PORTA = 3000;
+
+// Permite ao Express receber JSON enviado pelo front-end.
+app.use(express.json());
+
+app.use("/api/produtos", produtoRoutes);
+
+
+
+//PONTO FINAL DO SERVIDOR
+app.listen(PORTA, () => {
+  console.log(`Servidor executando em http://localhost:${PORTA}`);
+});
+
+
+
