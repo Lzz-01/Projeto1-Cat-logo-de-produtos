b97c041eff89f943cc0743593970469df109ce59
Aluno <e023aluno@outlook.com>
2026-08-12 21:58:10 -0300

    Conclusão do Formulário

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import FormProduto from "./components/FormProduto";
+export default function App(){
+
+  return(
+    <>
+    <main className="Conteiner">
+    <FormProduto />
+    </main>
+    </>
+  )
+}
